7cc702d96ab0406bb48b1d06e6afb8a5145ee443
ricky.nguyen <ricky.nguyen@starixsoft.com>
2019-12-17 12:49:55 +0700

    0000 - add another structure for component

---
+++ components/footer/__tests__/footer.spec.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import {mount} from 'enzyme';
+import Footer from '../footer';
+
+describe('Components', () => {
+  describe('Header', () => {
+    it('should render without throwing an error', function () {
+      const wrap = mount(<Footer/>);
+
+      expect(wrap.find('a').first().text()).toBe('Terms');
+    })
+  })  
+})
