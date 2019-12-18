92825845df0840a0d9bcaa4dd816db16e23810d3
ricky.nguyen <ricky.nguyen@starixsoft.com>
2019-12-18 10:50:33 +0700

    0000 - change naming convention

--- components/shared/header/Header.spec.tsx
+++ components/shared/header/Header.spec.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import {mount} from 'enzyme';
-import Header from '../Header';
+import Header from './Header';
 
 describe('Components', () => {
   describe('Header', () => {
