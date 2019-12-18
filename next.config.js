d07bca86d68877de34b682e80081433d19cbe4b5
alvin.nguyen <alvin.nguyen@nexcel.vn>
2019-12-18 14:01:18 +0700

    00000 - add next-offline and temporary favicon

--- next.config.js
+++ next.config.js
@@ -1,10 +1,37 @@
-const withSass = require('@zeit/next-sass')
-const env = process.env.SETTINGENV || 'development';
-const appSettings = { ...require('./appsettings.json'), ...require(`./appsettings.${env}.json`)};
-
-
-module.exports = withSass({
-  /* config options here */
-  excludeFile: (str) => /\*.{spec,test}.js/.test(str),
+const withPlugins = require("next-compose-plugins");
+const sass = require("@zeit/next-sass");
+const offline = require("next-offline");
+const env = process.env.SETTINGENV || "development";
+const appSettings = {
+  ...require("./appsettings.json"),
+  ...require(`./appsettings.${env}.json`)
+};
+const nextConfig = {
+  excludeFile: str => /\*.{spec,test}.js/.test(str),
   publicRuntimeConfig: appSettings
-});
+};
+
+module.exports = withPlugins(
+  [
+    sass,
+    [
+      offline,
+      {
+        workboxOpts: {
+          swDest: "static/service-worker.js"
+        },
+        experimental: {
+          async rewrites() {
+            return [
+              {
+                source: "/service-worker.js",
+                destination: "/_next/static/service-worker.js"
+              }
+            ];
+          }
+        }
+      }
+    ]
+  ],
+  nextConfig
+);
