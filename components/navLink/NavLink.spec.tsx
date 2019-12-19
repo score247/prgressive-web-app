8ac88e71357554e9b71eb18d9c4317040bca0340
Tyler Dang <tyler.dang@nexcel.vn>
2019-12-19 16:33:45 +0700

    125809 - Function bar

---
+++ components/navLink/NavLink.spec.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { mount } from 'enzyme';
+import NavLink from './index';
+
+describe('Components', () => {
+  describe('NavLink', () => {
+    it('should render without throwing an error', function() {});
+  });
+});
