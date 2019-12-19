6ade6b90ebe6544820b9667e287374c0e7789ebe
ricky.nguyen <ricky.nguyen@starixsoft.com>
2019-12-19 16:24:53 +0700

    125680 - update build script

--- _deployscripts/build.sh
+++ _deployscripts/build.sh
@@ -1,3 +1,4 @@
+cd /var/www/score247-web/main/
 sudo npm install
 sudo npm run build:production
 sudo pm2 restart score247-web-main
--- _deployscripts/build1.sh
+++
@@ -1,3 +0,0 @@
-sudo npm install
-sudo npm run build:production
-sudo pm2 restart score247-web-main
