3b707011a2567f1d943f57fcadffb1df5216c5cc
Theresa Le <theresa.le@nexcel.vn>
2019-12-23 10:47:55 +0700

    125721 - Add icon menu

--- components/shared/layout/header/navbar/NavbarMobile.tsx
+++ components/shared/layout/header/navbar/NavbarMobile.tsx
@@ -15,10 +15,10 @@ const NavbarMobile: React.FunctionComponent<IProps> = ({ sport }) => (
   <nav className="nav-menu-mobile">
     <ul className="menu">
       {createLink(`/${sport}`, "active", "menu-item", "Scores", detectIconClassName(sport))}
-      {createLink(`/${sport}/favorites`, "active", "menu-item", "Favorites", "icon-favorites")}
-      {createLink(`/${sport}/leagues`, "active", "menu-item", "Leagues", "icon-leagues")}
-      {createLink(`/${sport}/news`, "active", "menu-item", "News", "icon-news")}
-      {createLink(`/${sport}/tv`, "active", "menu-item", "TV", "icon-tv")}
+      {createLink(`/${sport}/favorites`, "active", "menu-item", "Favorites", "icon-menu-favorites")}
+      {createLink(`/${sport}/leagues`, "active", "menu-item", "Leagues", "icon-menu-leagues")}
+      {createLink(`/${sport}/news`, "active", "menu-item", "News", "icon-menu-news")}
+      {createLink(`/${sport}/tv`, "active", "menu-item", "TV", "icon-menu-tv")}
     </ul>
   </nav>
 );
