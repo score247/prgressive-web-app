96ebb3219e2532881a51855cfc4b5698b3067f4a
Theresa Le <theresa.le@nexcel.vn>
2019-12-30 14:07:56 +0700

    000 - Update date picker position

--- components/date-bar/index.tsx
+++ components/date-bar/index.tsx
@@ -98,7 +98,7 @@ class DateBar extends Component<Props, State> {
             locale={this.props.i18n.language}
             showPopperArrow={false}
             className={this.getDatePickerClassName()}
-            popperPlacement="right-bottom"
+            popperPlacement="end-right"
           />
         </div>
       </div>
