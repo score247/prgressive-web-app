4286e31407e4ddd31d33f51e1a0a96772b733225
alvin.nguyen <alvin.nguyen@nexcel.vn>
2020-01-14 08:50:48 +0700

    00000 - refactor

--- components/soccer/table/header/header.tsx
+++ components/soccer/table/header/header.tsx
@@ -2,40 +2,27 @@ import React from "react";
 import { DeviceContextConsumer } from "../../../../contexts/device-context";
 
 const Header = () => {
-  const mobileHeader = () => (
+  const header = ({ isMobile }: { isMobile: boolean }) => (
     <thead>
       <tr>
-        <th>League</th>
-        <th>Time</th>
-        <th>Home</th>
-        <th>Score</th>
-        <th>Away</th>
-        <th></th>
-      </tr>
-    </thead>
-  );
-
-  const desktopHeader = () => (
-    <thead>
-      <tr>
-        <th></th>
+        {!isMobile && <th></th>}
         <th>League</th>
         <th>Time</th>
         <th>Status</th>
         <th>Home</th>
         <th>Score</th>
         <th>Away</th>
-        <th className="width-50">1H</th>
-        <th></th>
+        {!isMobile && (
+          <>
+            <th className="width-50">1H</th>
+            <th></th>
+          </>
+        )}
       </tr>
     </thead>
   );
 
-  return (
-    <DeviceContextConsumer>
-      {({ isMobile }) => (isMobile ? mobileHeader() : desktopHeader())}
-    </DeviceContextConsumer>
-  );
+  return <DeviceContextConsumer>{header}</DeviceContextConsumer>;
 };
 
 export default Header;
