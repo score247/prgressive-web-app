ebf75a1825a85d694a18d96347a7deabf1ae3fe9
Faisal Tran <faisall.tran@nexcel.vn>
2020-01-21 13:28:51 +0700

    000 - html for extra text

--- pages/basketball/index.tsx
+++ pages/basketball/index.tsx
@@ -8,6 +8,35 @@ const Basketball: LocalizedPage = props => {
   return (
     <Layout title="Basketball" breadcrumbs={[props.t(ResourceKey.BASKETBALL)]}>
       <h1>{props.t(ResourceKey.BASKETBALL)}</h1>
+      <table>
+        <thead>
+          <th></th>
+          <th>League</th>
+          <th>Time</th>
+          <th>Status</th>
+          <th>Home</th>
+          <th>Score</th>
+          <th>Away</th>
+          <th>1H</th>
+          <th>Favorite</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td rowSpan={2}>checkbox</td>
+            <td rowSpan={2}>AUS D1</td>
+            <td rowSpan={2}>03:30</td>
+            <td rowSpan={2}></td>
+            <td>Sepidrood Rasht</td>
+            <td> 2 - 2</td>
+            <td>Baderan Tehran FC</td>
+            <td rowSpan={2}></td>
+            <td rowSpan={2}></td>
+          </tr>
+          <tr>
+            <td colSpan={3}>FT[1 - 1], AET[1 - 1], AGG[3 - 3], PEN[5 - 4]</td>
+          </tr>
+        </tbody>
+      </table>
     </Layout>
   );
 };
