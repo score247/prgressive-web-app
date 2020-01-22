743da672692fefcf38a5199f8f6e2600616002ff
Elton.Ton <eton.ton@nexcel.vn>
2020-01-22 13:59:38 +0700

    000 - soccer detail page - swipe on mobile

--- components/soccer/match-detail/mobile-view/index.tsx
+++ components/soccer/match-detail/mobile-view/index.tsx
@@ -2,9 +2,27 @@ import React, { useState } from "react";
 import GeneralInfo from "../general-info";
 import { MatchInfo } from "../../../../models/match-info";
 import { Tabs, TabList, Tab, TabPanel } from "react-tabs";
+import SwipeableViews from "react-swipeable-views";
 
 type Props = {
   matchInfo: MatchInfo;
+};
+
+const styles = {
+  slide: {
+    padding: 15,
+    minHeight: 100,
+    color: "#fff"
+  },
+  slide1: {
+    backgroundColor: "#FEA900"
+  },
+  slide2: {
+    backgroundColor: "#B3DC4A"
+  },
+  slide3: {
+    backgroundColor: "#6AC0FF"
+  }
 };
 const Mobile: React.FC<Props> = props => {
   const [selectedTabIndex, setTab] = useState(0);
@@ -21,15 +39,28 @@ const Mobile: React.FC<Props> = props => {
           <Tab>H2H</Tab>
           <Tab>Table</Tab>
         </TabList>
-        <TabPanel>Info</TabPanel>
-        <TabPanel>Tracker</TabPanel>
-        <TabPanel>Stats</TabPanel>
-        <TabPanel>Line-ups</TabPanel>
-
-        <TabPanel>H2H</TabPanel>
-
-        <TabPanel>Table</TabPanel>
+        <SwipeableViews index={selectedTabIndex} onChangeIndex={setTab} enableMouseEvents>
+          <TabPanel style={Object.assign({}, styles.slide, styles.slide1)}>
+            Info
+          </TabPanel>
+          <TabPanel style={Object.assign({}, styles.slide, styles.slide2)}>
+            Tracker
+          </TabPanel>
+          <TabPanel style={Object.assign({}, styles.slide, styles.slide3)}>
+            Stats
+          </TabPanel>
+          <TabPanel style={Object.assign({}, styles.slide, styles.slide1)}>
+            Line-ups
+          </TabPanel>
+          <TabPanel style={Object.assign({}, styles.slide, styles.slide2)}>
+            H2H
+          </TabPanel>
+          <TabPanel style={Object.assign({}, styles.slide, styles.slide3)}>
+            Table
+          </TabPanel>
+        </SwipeableViews>
       </Tabs>
+     
     </>
   );
 };
