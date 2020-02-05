daa3c40cbe8de55fc21615931f8acf4fcefd0fc1
Faisal Tran <faisall.tran@nexcel.vn>
2020-02-05 16:24:03 +0700

    000 - loading page

--- components/layout/header/navbar/navtest.tsx
+++ components/layout/header/navbar/navtest.tsx
@@ -12,7 +12,17 @@ const NavTest: React.FunctionComponent<IProps> = (props) => {
     }
 
     return (
-        <input type="button" value="soccer" onClick={handleClick} />
+        <div className="loading-page">
+            <div className="title">Score247 Official Website</div>
+            <div className="note">You are using a smart device. Please select one of the following browsing mode that's compatible with your device</div>
+            <div className="content">
+                <button className="btn-mobile">Mobile website</button>
+                <button className="btn-desktop">Desktop website</button>
+                <div className="divide">or mobile application</div>
+                <button className="btn-download">Download from app store</button>
+                <div className="logo"></div>
+            </div>
+        </div>
     );
 };
 
--- pages/loading.tsx
+++ pages/loading.tsx
@@ -4,6 +4,7 @@ import cookie from 'js-cookie';
 import Router from 'next/router';
 import { ViewMode, ResourceType } from "../common/constants";
 import Button from "../components/basic/button";
+import "../assets/styles/pages/loading.scss";
 
 const LoadingPage: LocalizedPage = () => {
 
@@ -12,9 +13,19 @@ const LoadingPage: LocalizedPage = () => {
         Router.push("/");
     };
     return (
-        <div>
-            <Button text="Desktop Mode" onClick={() => handleClick(ViewMode.Desktop)} />
-            <Button text="Mobile Mode" onClick={() => handleClick(ViewMode.Mobile)} />
+        <div className="loading-page">
+            <div className="title">Score247 Official Website</div>
+            <div className="note">You are using a smart device. Please select one of the following browsing mode that's compatible with your device</div>
+            <div className="content">
+                <button className="btn btn-mobile">Mobile website</button>
+                <button className="btn btn-primary-outline btn-desktop">Desktop website</button>
+                <div className="divide">or mobile application</div>
+                <button className="btn btn-download">Download from app store</button>
+                <div className="icon-logo"></div>
+            </div>
+            <div className="footer">
+                <span className="icon-copy-right"></span>2019 Score247, Inc. All right reserved 
+            </div>
         </div>
     );
 };
