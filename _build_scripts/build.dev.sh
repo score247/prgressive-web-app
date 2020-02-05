901cc020f4484dee837cddc8728d8f58240cf400
ricky.nguyen <ricky.nguyen@starixsoft.com>
2020-02-05 16:49:40 +0700

    127968 - update build scripts

--- _build_scripts/build.dev.sh
+++ _build_scripts/build.dev.sh
@@ -1,3 +1,4 @@
+cd /var/www/score247-web/dev/
 sudo npm install
 sudo npm run build:production
 sudo pm2 restart score247-web-dev
--- _build_scripts/build.test.sh
+++ _build_scripts/build.test.sh
@@ -1,3 +1,4 @@
+cd /var/www/score247-web/test/
 sudo npm install
 sudo npm run build:production
 sudo pm2 restart score247-web-test
