2901ec2e16cbd9088ffa47dbcd9fb2e112c4fc5c
alvin.nguyen <alvin.nguyen@nexcel.vn>
2020-02-07 16:54:15 +0700

    127948 - search bar

--- components/search-bar/index.tsx
+++ components/search-bar/index.tsx
@@ -1,71 +1,19 @@
-import React from "react";
-import { SearchBarProps } from "./type";
 import "./style.scss";
+import React from "react";
+import SearchBox from "./search-box";
+import MobileSearchBox from "./mobile-search-box";
+import { SearchBarProps } from "./type";
 import { DeviceContextConsumer } from "../../contexts/device-context";
 
-type State = {
-  openSearch: boolean;
-};
-
-class MobileSearchBox extends React.Component<SearchBarProps, State> {
-  constructor(props: SearchBarProps) {
-    super(props);
-    this.state = {
-      openSearch: false
-    };
-  }
-
-  render() {
-    return (
-      <div className="search-section">
-        {this.state.openSearch
-          ? (<>
-            <div className="search-box">
-              <span className="icon-search"></span>
-              <input
-                type="text"
-                placeholder="Search"
-                value={this.props.filterText}
-                onChange={(event) => { this.props.onFilterTextChange(event.target.value); }}
-                className="txt-search"
-              />
-            </div>
-            <span className="btn-cancel" onClick={() => { this.setState({ openSearch: false }); }}>Cancel</span>
-          </>)
-          : (<span className="icon-search" onClick={() => { this.setState({ openSearch: true }); }}></span>)
-        }
-      </div>
-    );
-  }
-}
-
 const SearchBar: React.FC<SearchBarProps> = props => {
-  const handleFilterTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    props.onFilterTextChange(event.target.value);
-  };
-
-  const DesktopSearchBox = () => {
-    return (
-      <div className="search-section">
-        <div className="search-box">
-          <span className="icon-search"></span>
-          <input
-            type="text"
-            placeholder="Search"
-            value={props.filterText}
-            onChange={handleFilterTextChange}
-            className="txt-search"
-          />
-        </div>
-      </div>
-    );
+  const searchBox = ({ isMobile }: { isMobile: boolean }) => {
+    return (isMobile ? <MobileSearchBox {...props} /> : <SearchBox {...props} />);
   };
 
   return (
-    <DeviceContextConsumer>
-      {({ isMobile }) => (!isMobile ? DesktopSearchBox : <MobileSearchBox {...props} />)}
-    </DeviceContextConsumer>
-
+    <div className="search-section">
+      <DeviceContextConsumer>{searchBox}</DeviceContextConsumer>
+    </div>
   );
 };
 
---
+++ components/search-bar/search-box.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { SearchBarProps } from "./type";
+
+const SearchBox: React.FC<SearchBarProps> = props => {
+    const handleFilterTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        props.onFilterTextChange(event.target.value);
+    };
+
+    return (
+        <div className="search-box">
+            <span className="icon-search"></span>
+            <input
+                type="text"
+                placeholder="Search"
+                value={props.filterText}
+                onChange={handleFilterTextChange}
+                className="txt-search"
+            />
+        </div>
+    );
+};
+
+export default SearchBox;
