ad1bb0e836b9211920420a8e59459e911dd4adcd
Elton.Ton <eton.ton@nexcel.vn>
2020-02-10 09:56:00 +0700

    000 - match detail - set interval to update match status

--- components/soccer/match-detail/general-info/status/index.tsx
+++ components/soccer/match-detail/general-info/status/index.tsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Props } from "./type";
 import { buildMatchStatus } from "../../../../../common/helpers/match-status-helper";
 
 const Status: React.FC<Props> = props => {
   const { match } = props;
+  const [status, setStatus] = useState<string>(buildMatchStatus(match));
+  const intervalId = window.setInterval(
+    () => setStatus(buildMatchStatus(match)),
+    60000 //NOSONAR
+  );
 
-  return <div className="status">{buildMatchStatus(match)}</div>;
+  useEffect(() => {
+    return () => window.clearInterval(intervalId);
+  });
+
+  return <div className="status">{status}</div>;
 };
 
 export default Status;
